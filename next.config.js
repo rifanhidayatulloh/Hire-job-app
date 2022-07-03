e792107793e8a70c25b500de2b39028a1991f887
rifanhidayatulloh <rifanhidayatulloh96@gmail.com>
2022-07-03 14:24:33 +0700

    menambahkan config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['hirejob-web-app.herokuapp.com', 'unsplash.com', 'images.unsplash.com']
+    domains: ['hirejob-web-app.herokuapp.com', 'unsplash.com', 'images.unsplash.com', 'drive.google.com']
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.BACKEND_URL
